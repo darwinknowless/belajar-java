48a5ab666d19ee77b3c14946e3320510e120df4f
Darwin <id.darwins@gmail.com>
2021-06-21 04:02:36 +0700

    Add Belajar JAVA

---
+++ src/Latihan01/DataDiri.java
@@ -0,0 +1,23 @@
+package Latihan01;
+
+public class DataDiri {
+
+	public static void main(String[] args) {
+		// Membuat variabel
+		String nama, alamat;
+		int usia;
+		double tinggi;
+
+		// Mengisi variabel
+		nama = "Darwin";
+		alamat = "Jakarta Utara";
+		usia = 29;
+		tinggi = 170.5;
+
+		// Mencetak ke layar isi variabel
+		System.out.println("Nama: " + nama);
+		System.out.println("Alamat: " + alamat);
+		System.out.println("Usia: " + usia + " Tahun");
+		System.out.println("Tinggi: " + tinggi + " cm");
+	}
+}
---
+++ src/Latihan01/LuasLingkaran.java
@@ -0,0 +1,21 @@
+package Latihan01;
+
+public class LuasLingkaran {
+
+	public static void main(String[] args) {
+
+		// Deklarasi
+		Double pi, r, luas;
+
+		// Deskripsi
+		pi = 3.14;
+		r = 10.0;
+
+		// Proses
+		luas = pi * r * r;
+
+		// Output
+		System.out.println("Luas Lingkaran = ");
+		System.out.println(luas);
+	}
+}
