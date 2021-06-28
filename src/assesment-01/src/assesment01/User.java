d352c7cbbc40137cffc32cc35ead0a85c80b8db0
Darwin <id.darwins@gmail.com>
2021-06-28 17:18:04 +0000

    Update User.java

--- src/assesment-01/src/assesment01/User.java
+++ src/assesment-01/src/assesment01/User.java
@@ -1,4 +1,15 @@
 package assesment01;
+//1. Jelaskan konsep OOP (Inheritance, Polymorphism, Abstraction, Encapsulation) dengan bahasa sendiri
+//- Inheritance adalah kemampuan sebuah Class untuk dapat mewariskan atribut dan method kepada class lain atau sub class dibawahnya.;
+//- Polymorphism artinya banyak bentuk, atau penggunaan satu method dengan nama yang sama, polymorphism dapat dilakukan dengan menerapkan;
+//    a. Overriding : berarti mendefinisikan method superclass dengan nama yang sama pada subclass ;
+//    b. Overloading : Overloading artinya menerapkan method yang sama namun dengan parameter yang berbeda;
+//- Abstraction adalah class yang tidak bisa di instatiate atau di buat object baru secara langsung, hanya bisa di inheritance
+//- Encapsulation adalah sebuah metode untuk mengatur class agar atribut-atribut sensitive yang terdapat dalam sebuah class tidak dapat di
+//  akses dari luar kecuali dengan memanggil method tertentu seperti Set atau Get method dari sebuah class. ada 3 aksesbilitas dalam Encapsulation yaitu:
+//    a. public : properti dapat di akses dari manapun
+//    b. private : properti hanya dapat di akses dari dalam class
+//    c. protected : properti hanya dapat di akses dari dalam class atau class turunannya
 
 import java.util.Scanner;
 import encodeDecode.Main;
